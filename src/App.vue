f85ad951d434179d6b8b8e124665c5a7a85dd69c
971389117 <971389117@qq.com>
2019-07-16 00:53:03 +0800

    first4

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <Heador></Heador>
+    <div class="main">
+      <router-view class="app-layout" name="slidebar"></router-view>
+      <router-view name="main"></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Heador from './components/Header'
+  import PostList from './components/PostList'
+export default {
+  name: 'App',
+  components:{
+    Heador,PostList
+  }
+}
+</script>
+
+<style>
+.main{
+  width: 80%;
+  margin: 0 auto;
+}
+#app{
+  background: #e1e1e1;
+  min-height: 100vh;
+}
+.app-layout{
+  position: fixed;
+  right:0;
+  top:63px;
+  min-width: 450px;
+}
+body{
+  margin: 0;
+  padding: 0;
+}
+</style>
